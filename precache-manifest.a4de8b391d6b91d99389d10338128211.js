697a1c528e23e43741c16abb3dec3f6f729f9355
Ilya Aleshin <snypoon2202@gmail.com>
2020-07-10 18:02:04 +0300

    final

---
+++ precache-manifest.a4de8b391d6b91d99389d10338128211.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584ce004e75d7ec6bbd39a2bff529d7f",
+    "url": "/MyToDo-demo/index.html"
+  },
+  {
+    "revision": "35840670c6088b70f0aa",
+    "url": "/MyToDo-demo/static/css/main.e6fd66c2.chunk.css"
+  },
+  {
+    "revision": "b7e1bbac61171a9ad096",
+    "url": "/MyToDo-demo/static/js/2.55f289b5.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/MyToDo-demo/static/js/2.55f289b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35840670c6088b70f0aa",
+    "url": "/MyToDo-demo/static/js/main.a0e8699a.chunk.js"
+  },
+  {
+    "revision": "74209eaf2633e00cda96",
+    "url": "/MyToDo-demo/static/js/runtime-main.f10f19b3.js"
+  },
+  {
+    "revision": "b7350427f5d9fbceabb3325336d69531",
+    "url": "/MyToDo-demo/static/media/Ubuntu-Regular.b7350427.woff2"
+  },
+  {
+    "revision": "ba4e7809d474b7b6936d09346695c3ae",
+    "url": "/MyToDo-demo/static/media/Ubuntu-Regular.ba4e7809.woff"
+  }
+]);
